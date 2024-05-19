4489b73403fc96cc7c2897715477ee4f57b72157
Aishwarya-TM <127846109+Aishwarya-TM@users.noreply.github.com>
2024-05-19 16:45:41 +0530

    Update bookingData.js

--- data/bookingData.js
+++ data/bookingData.js
@@ -1,5 +1,4 @@
-const bookingData = 
-[
+const bookingData = [
     {
         "bookingId": "B001",
         "userId": "user1",
@@ -8,8 +7,8 @@ const bookingData =
         "price": 300,
         "paymentStatus": "Paid",
         "bookingDate": "2024-06-01T09:00:00Z"
-      },
-      {
+    },
+    {
         "bookingId": "B002",
         "userId": "user2",
         "flightId": "FL002",
@@ -17,8 +16,8 @@ const bookingData =
         "price": 250,
         "paymentStatus": "Paid",
         "bookingDate": "2024-06-02T10:00:00Z"
-      },
-      {
+    },
+    {
         "bookingId": "B003",
         "userId": "user1",
         "flightId": "FL003",
@@ -26,8 +25,8 @@ const bookingData =
         "price": 350,
         "paymentStatus": "Paid",
         "bookingDate": "2024-06-03T11:00:00Z"
-      },
-      {
+    },
+    {
         "bookingId": "B004",
         "userId": "user3",
         "flightId": "FL004",
@@ -35,8 +34,8 @@ const bookingData =
         "price": 280,
         "paymentStatus": "Paid",
         "bookingDate": "2024-06-04T12:00:00Z"
-      },
-      {
+    },
+    {
         "bookingId": "B005",
         "userId": "user2",
         "flightId": "FL005",
@@ -44,8 +43,8 @@ const bookingData =
         "price": 200,
         "paymentStatus": "Paid",
         "bookingDate": "2024-06-05T13:00:00Z"
-      },
-      {
+    },
+    {
         "bookingId": "B006",
         "userId": "user1",
         "flightId": "FL006",
@@ -53,8 +52,8 @@ const bookingData =
         "price": 400,
         "paymentStatus": "Paid",
         "bookingDate": "2024-06-06T14:00:00Z"
-      },
-      {
+    },
+    {
         "bookingId": "B007",
         "userId": "user3",
         "flightId": "FL007",
@@ -62,8 +61,8 @@ const bookingData =
         "price": 180,
         "paymentStatus": "Paid",
         "bookingDate": "2024-06-07T15:00:00Z"
-      },
-      {
+    },
+    {
         "bookingId": "B008",
         "userId": "user2",
         "flightId": "FL008",
@@ -71,8 +70,8 @@ const bookingData =
         "price": 220,
         "paymentStatus": "Paid",
         "bookingDate": "2024-06-08T16:00:00Z"
-      },
-      {
+    },
+    {
         "bookingId": "B009",
         "userId": "user1",
         "flightId": "FL009",
@@ -80,8 +79,8 @@ const bookingData =
         "price": 320,
         "paymentStatus": "Paid",
         "bookingDate": "2024-06-09T17:00:00Z"
-      },
-      {
+    },
+    {
         "bookingId": "B010",
         "userId": "user3",
         "flightId": "FL010",
@@ -89,7 +88,96 @@ const bookingData =
         "price": 250,
         "paymentStatus": "Paid",
         "bookingDate": "2024-06-10T18:00:00Z"
-      }
-]
-
+    },
+    {
+        "bookingId": "B011",
+        "userId": "user1",
+        "flightId": "FL011",
+        "seatNumber": "2A",
+        "price": 275,
+        "paymentStatus": "Paid",
+        "bookingDate": "2024-06-11T09:00:00Z"
+    },
+    {
+        "bookingId": "B012",
+        "userId": "user2",
+        "flightId": "FL012",
+        "seatNumber": "4B",
+        "price": 225,
+        "paymentStatus": "Paid",
+        "bookingDate": "2024-06-12T10:00:00Z"
+    },
+    {
+        "bookingId": "B013",
+        "userId": "user3",
+        "flightId": "FL013",
+        "seatNumber": "6C",
+        "price": 300,
+        "paymentStatus": "Paid",
+        "bookingDate": "2024-06-13T11:00:00Z"
+    },
+    {
+        "bookingId": "B014",
+        "userId": "user1",
+        "flightId": "FL014",
+        "seatNumber": "7D",
+        "price": 350,
+        "paymentStatus": "Paid",
+        "bookingDate": "2024-06-14T12:00:00Z"
+    },
+    {
+        "bookingId": "B015",
+        "userId": "user2",
+        "flightId": "FL015",
+        "seatNumber": "8E",
+        "price": 290,
+        "paymentStatus": "Paid",
+        "bookingDate": "2024-06-15T13:00:00Z"
+    },
+    {
+        "bookingId": "B016",
+        "userId": "user3",
+        "flightId": "FL016",
+        "seatNumber": "9F",
+        "price": 310,
+        "paymentStatus": "Paid",
+        "bookingDate": "2024-06-16T14:00:00Z"
+    },
+    {
+        "bookingId": "B017",
+        "userId": "user1",
+        "flightId": "FL017",
+        "seatNumber": "10A",
+        "price": 330,
+        "paymentStatus": "Paid",
+        "bookingDate": "2024-06-17T15:00:00Z"
+    },
+    {
+        "bookingId": "B018",
+        "userId": "user2",
+        "flightId": "FL018",
+        "seatNumber": "11B",
+        "price": 210,
+        "paymentStatus": "Paid",
+        "bookingDate": "2024-06-18T16:00:00Z"
+    },
+    {
+        "bookingId": "B019",
+        "userId": "user3",
+        "flightId": "FL019",
+        "seatNumber": "12C",
+        "price": 245,
+        "paymentStatus": "Paid",
+        "bookingDate": "2024-06-19T17:00:00Z"
+    },
+    {
+        "bookingId": "B020",
+        "userId": "user1",
+        "flightId": "FL020",
+        "seatNumber": "13D",
+        "price": 260,
+        "paymentStatus": "Paid",
+        "bookingDate": "2024-06-20T18:00:00Z"
+    }
+];
 module.exports = bookingData
