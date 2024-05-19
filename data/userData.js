611717f45c4846a71eedf24b32fe2f0caea9fcfc
Aishwarya-TM <127846109+Aishwarya-TM@users.noreply.github.com>
2024-05-19 16:48:49 +0530

    Update userData.js

--- data/userData.js
+++ data/userData.js
@@ -1,33 +1,214 @@
-const userData = 
-[
-    {
-        "username": "user1",
-        "password": "password1",
-        "email": "user1@example.com",
-        "role": "User"
-      },
-      {
-        "username": "user2",
-        "password": "password2",
-        "email": "user2@example.com",
-        "role": "User"
-      },
-      {
-        "username": "user3",
-        "password": "password3",
-        "email": "user3@example.com",
-        "role": "User"
-      },
-      {
-        "username": "admin1",
-        "password": "adminpassword1",
-        "email": "admin1@example.com",
-        "role": "Admin"
-      },
-      {
-        "username": "admin2",
-        "password": "adminpassword2",
-        "email": "admin2@example.com",
-        "role": "Admin"
-      }
-]
+const userData = [
+  {
+    "userId": 1,
+    "username": "user1",
+    "password": "password1",
+    "email": "user1@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 2,
+    "username": "user2",
+    "password": "password2",
+    "email": "user2@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 3,
+    "username": "user3",
+    "password": "password3",
+    "email": "user3@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 4,
+    "username": "admin1",
+    "password": "adminpassword1",
+    "email": "admin1@example.com",
+    "role": "Admin"
+  },
+  {
+    "userId": 5,
+    "username": "admin2",
+    "password": "adminpassword2",
+    "email": "admin2@example.com",
+    "role": "Admin"
+  },
+  {
+    "userId": 6,
+    "username": "user4",
+    "password": "password4",
+    "email": "user4@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 7,
+    "username": "user5",
+    "password": "password5",
+    "email": "user5@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 8,
+    "username": "user6",
+    "password": "password6",
+    "email": "user6@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 9,
+    "username": "user7",
+    "password": "password7",
+    "email": "user7@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 10,
+    "username": "user8",
+    "password": "password8",
+    "email": "user8@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 11,
+    "username": "user9",
+    "password": "password9",
+    "email": "user9@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 12,
+    "username": "user10",
+    "password": "password10",
+    "email": "user10@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 13,
+    "username": "user11",
+    "password": "password11",
+    "email": "user11@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 14,
+    "username": "user12",
+    "password": "password12",
+    "email": "user12@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 15,
+    "username": "user13",
+    "password": "password13",
+    "email": "user13@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 16,
+    "username": "user14",
+    "password": "password14",
+    "email": "user14@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 17,
+    "username": "user15",
+    "password": "password15",
+    "email": "user15@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 18,
+    "username": "user16",
+    "password": "password16",
+    "email": "user16@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 19,
+    "username": "user17",
+    "password": "password17",
+    "email": "user17@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 20,
+    "username": "user18",
+    "password": "password18",
+    "email": "user18@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 21,
+    "username": "user19",
+    "password": "password19",
+    "email": "user19@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 22,
+    "username": "user20",
+    "password": "password20",
+    "email": "user20@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 23,
+    "username": "user21",
+    "password": "password21",
+    "email": "user21@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 24,
+    "username": "user22",
+    "password": "password22",
+    "email": "user22@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 25,
+    "username": "user23",
+    "password": "password23",
+    "email": "user23@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 26,
+    "username": "user24",
+    "password": "password24",
+    "email": "user24@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 27,
+    "username": "user25",
+    "password": "password25",
+    "email": "user25@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 28,
+    "username": "user26",
+    "password": "password26",
+    "email": "user26@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 29,
+    "username": "user27",
+    "password": "password27",
+    "email": "user27@example.com",
+    "role": "User"
+  },
+  {
+    "userId": 30,
+    "username": "user28",
+    "password": "password28",
+    "email": "user28@example.com",
+    "role": "User"
+  }
+];
+
+module.exports = userData;
