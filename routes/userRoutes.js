8471fc485569e86e30afb0a2dbab1598eab0d12e
Aishwarya-TM <127846109+Aishwarya-TM@users.noreply.github.com>
2024-05-19 16:51:10 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { userSignup, userLogin, searchFlight, flightBooking, userBookingDetails } = require('../controllers/userController')
+const { userSignup, userLogin, searchFlight, flightBooking, userBookingDetails, getAllFlightDetails } = require('../controllers/userController')
 
 router.post('/signup', userSignup)
 router.post('/login', userLogin)
+router.get('/get', getAllFlightDetails)
 router.get('/search', searchFlight)
 router.post('/book', flightBooking)
 router.get('/my-bookings/:userId', userBookingDetails)
