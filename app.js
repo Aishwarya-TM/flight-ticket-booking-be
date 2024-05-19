c54779491c8099df2a9668edbdc42c1d448e5cc0
Aishwarya-TM <127846109+Aishwarya-TM@users.noreply.github.com>
2024-05-19 16:50:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const PORT = 3500
-const cors = require('cors')
-const mongoose = require('mongoose')
-const userRoutes = require('./routes/userRoutes')
-const adminRoutes = require('./routes/adminRoutes')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = 3500;
+const cors = require('cors');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const flightData = require('./data/flightData');
+const Flight = require('./models/flightModel');
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/',(request, response)=>
-{
-    response.status(200).send({message : "hi!"})
+app.get('/', (request, response) => {
+  response.status(200).send({ message: "hi!" });
 })
 
-mongoose.connect(process.env.DB_URL)
-const db = mongoose.connection
-db.on('error', (errorMessage) => console.log(errorMessage))
-db.once('open', () => console.log('Connected to db successfully!'))
+mongoose.connect(process.env.DB_URL);
 
-app.use('/api/v1/flights', adminRoutes )
-app.use('/api/v1/flights', userRoutes )
+const db = mongoose.connection;
+db.on('error', (errorMessage) => console.log(errorMessage));
+db.once('open', () => {
+  console.log('Connected to db successfully!');
+})
 
+app.use('/api/v1/admin/flights', adminRoutes);
+app.use('/api/v1/users/flights', userRoutes);
 
-app.listen(PORT, () =>
-{
-    console.log(`server is running at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 })
